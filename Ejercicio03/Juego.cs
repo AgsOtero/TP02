4bf9d6ce8470ec203c215fb473bee2c811cd520c
agustin otero <oteroagustin95@gmail.com>
2019-09-19 12:08:41 -0300

    19/09

--- Ejercicio03/Juego.cs
+++ Ejercicio03/Juego.cs
@@ -8,5 +8,54 @@ namespace Ejercicio03
 {
     class Juego
     {
+        private List<Partida> iListaPartidas;
+
+        public Juego()
+        {
+            this.iListaPartidas = new List<Partida>();
+        }
     }
+    public void FinalizarPartida(Partida pPartida)
+    {
+        this.iListaPartida.Add(pPartida);
+        OrdenarListaPartidas(this.iListaPartidas);
+    }
+    private void OrdenarListaPartidas(Lista<Partidas> pLista)
+    {
+        this.iListaPartidas = Orderby(o => o.iDuracion).ToList();
+    }
+    private List <Partida> FiltrarGanadores(List<Partida> pLista)
+    {
+        List<Partida> partidasGanadoras = new List<Partida>();
+        if (pLista.Count>0)
+        {
+            for (int i=0;i<pLista.Count;i++)
+            {
+                if (pLista[i].Gano())
+                {
+                    partidasGanadoras.Add(pLista[i]);
+                }
+            }
+        }
+        return (PartidasGanadoras);
+    }
+    public List <Partida> PartidasGanadoras(List<Partidas> pLista)
+    {
+        List<Partida> mejoresTiempos = new List<Partida>();
+        List<Partida> partidasGanadoras = this.FiltrarGanadores(this.iListaPartidas);
+        if (partidasGanadoras.Count >= 5)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                mejoresTiempos.Add(partidasGanadoras[i]);
+            }
+
+        }
+        else
+        {
+            return (partidasGanadoras);
+        }
+        return (mejoresTiempos);
+    }
+       
 }
--- Ejercicio03/Ventana.cs
+++ Ejercicio03/Ventana.cs
@@ -8,5 +8,86 @@ namespace Ejercicio03
 {
     class Ventana
     {
+        private ControladorDeJuego iControlador;
+        private List<Partida> iPartidas;
+        private List<Char> iLetrasProbadas;
+
+        public Ventana(ControladorDeJuego pControlador)
+        {
+            this.iControlador = pControlador;
+        }
+        public void Principal()
+        {
+            Console.Clear();
+            Console.WriteLine("JUEGO");
+            Console.WriteLine();
+            Console.WriteLine("1.Nueva Partida");
+            Console.WriteLine("2.Lista Mejores Partidas");
+
+
+        }
+        public void SelecionarNombre ()
+        {
+            Console.Clear();
+            Console.Write("Jugador: ");
+        }
+        public void SeleccionarCantidadIntentos()
+        {
+            Console.Write("Cantidad de Fallos Permitidos: ");
+        }
+        public void Partida()
+        {
+            Console.Clear();
+            Console.WriteLine(iControlador.DevolverPalabraFormada());
+            Console.WriteLine();
+            Console.WriteLine("Elija una letra");
+            Console.WriteLine();
+            this.iLetrasProbadas = iControlador.LetrasProbadas();
+            if (this.iLetrasProbadas.Count > 0)
+            {
+                Console.WriteLine("Letras ya probadas ");
+                for (int i=0;i<this.iLetrasProbadas.Count;i++)
+                {
+                    Console.WriteLine($" {this.iLetrasProbadas[i]}");
+                }
+            }
+        }
+        public void LetraYaElegida()
+        {
+            Console.Clear();
+            Console.WriteLine("La letra seleccionada ya ha sido elegida");
+            Console.WriteLine("Toque cualquier tecla para continuar");
+            Console.ReadLine();
+
+        }
+        public void Gano()
+        {
+            Console.Clear();
+            Console.WriteLine($"GANO!!!La palabra era: {iControlador.DevolverPalabraFormada()}");
+
+        }
+        public bool MejoresPartidas()
+        {
+            Console.Clear();
+            this.iPartidas = this.iControlador.ListaMejoresPartidas();
+            if (this.iPartidas.Count>0)
+            {
+                for (int i=0;i<this.iPartidas.Count;i++)
+                {
+                    Console.WriteLine($"Jugador: {iPartidas[i].Jugador}  Tiempo: {iPartidas[i].Duracion}");
+                }
+                return (true);
+                }
+            else
+            {
+                return (false);
+            }
+        }
+
+        public void NoHayPartidasJugadas()
+        {
+            Console.Clear();
+            Console.WriteLine("No hay partidas jugadas");
+        }
     }
 }
